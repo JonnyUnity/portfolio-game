fccdc45587318358c1e56d31cdb75f1842fb93ee
Jonny <95136177+JonnyUnity@users.noreply.github.com>
2021-11-29 21:38:22 +0000

    Player and follow cam added. Added ground and some obstacles

---
+++ Portfolio Game/Assets/Scripts/camera_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_script : MonoBehaviour
+{
+    private Vector3 Offset;
+    [SerializeField]
+    private Transform playerTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Offset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + Offset;
+    }
+}
---
+++ Portfolio Game/Assets/Scripts/player_script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_script : MonoBehaviour
+{
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    [SerializeField]
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float speed = 10f;
+    private float sprintSpeed = 50f;
+    private float jumpSpeed = 8f;
+
+
+    //For testing the player is on the ground
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    bool isJumping = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.Move(forward * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = jumpSpeed;
+            //isJumping = true;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+}
