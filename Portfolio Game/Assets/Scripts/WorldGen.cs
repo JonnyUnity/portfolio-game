a1bb88ecd6b392f951f6a6613fb2eb8b4c019efb
Jonny <95136177+JonnyUnity@users.noreply.github.com>
2021-12-04 20:36:30 +0000

    Added "DoubleHurdleGround" prefab. Added WorldGen script to add a DoubleHurdleGround prefab when starting the game.

---
+++ Portfolio Game/Assets/Scripts/WorldGen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+
+    public GameObject groundPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(groundPrefab, new Vector3(0f, 0f, 45f), Quaternion.identity);
+    }
+
+
+}
