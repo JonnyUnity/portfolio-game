125e9b042f15784b481bb9088335a20bcdb7d5ac
Jonny <95136177+JonnyUnity@users.noreply.github.com>
2021-12-06 08:50:35 +0000

    Adding simple UI Menus

---
+++ Portfolio Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Canvas canvas;
+
+    public void StartGame()
+    {
+        Debug.Log("Game Started!");
+        canvas.enabled = false;
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+}
